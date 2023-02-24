c5d3b1593865479bb0539d9a06be96c2a1f9ca7d
jasper3357 <jasper3357@hotmail.com>
2023-02-24 12:01:31 +0100

    brand carousel  done

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,10 +5,12 @@ import Splide from '@splidejs/splide';
 new Splide( '.splide',{
   type: 'loop',
   perPage: 4,
-  gap: '2rem',
+  gap: '3vw',
   perMove: 1,
   autoplay: true,
   interval: 10000,
+  pagination: false,
+  arrows: true,
 } ).mount();
  
 window.Alpine = Alpine
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,12 +3,15 @@
 @section('title')Home @endsection
 
 @section('content')
+@php
+  $brands = ["apple", "samsung", "huawei", "google", "xiaomi", "oneplus", "motorola", "microsoft", "oppo", "honor"];
+@endphp
 <section class="homebg text-mainWhite">
-  <div class="container space mx-auto py-16">
-    <div class="flex flex-row justify-between items-center gap-2">
+  <div class="container py-16 mx-auto space">
+    <div class="flex flex-row items-center justify-between gap-2">
       <div class="max-w-[50%] flex flex-col gap-16">
-        <h1 class="font-bold text-5xl w-3/4">The New PulsarNL Collection is Here!</h1>
-        <a href="" class="py-2 bg-mainPurple hover:bg-altPurple transition-colors ease-in-out rounded-full w-1/3 text-center font-bold text-lg">Check it Out</a>
+        <h1 class="w-3/4 text-5xl font-bold">The New PulsarNL Collection is Here!</h1>
+        <a href="" class="w-1/3 py-2 text-lg font-bold text-center transition-colors ease-in-out rounded-full bg-mainPurple hover:bg-altPurple">Check it Out</a>
       </div>
       <div class="max-w-[50%]">
         <img src="{{ asset('media/products/cable-usbc-usba.png') }}" class="h-[400px]" alt="PulsarNL usb-c to usb-a cable">
@@ -17,26 +20,27 @@
   </div>
 </section>
 <section class="bg-mainWhite">
-  <div class="container space mx-auto py-12">
-    <h3 class="font-bold text-xl">Check out our brands</h3>
+  <div class="container py-12 mx-auto space">
+    <h3 class="mb-8 text-xl font-bold">Check out our brands</h3>
     <div class="splide" role="group" aria-label="Splide Basic HTML Example">
       <div class="splide__track">
-        <ul class="splide__list">
-          <li class="splide__slide p-6 bg-offWhite">Apple</li>
-          <li class="splide__slide p-6 bg-offWhite">Samsung</li>
-          <li class="splide__slide p-6 bg-offWhite">Huawei</li>
-          <li class="splide__slide p-6 bg-offWhite">Google</li>
-          <li class="splide__slide p-6 bg-offWhite">Xiaomi</li>
-          <li class="splide__slide p-6 bg-offWhite">OnePlus</li>
-          <li class="splide__slide p-6 bg-offWhite">Microsoft</li>
+        <ul class="text-center splide__list">
+          @foreach ($brands as $brand)
+            @php $img = $brand.'.png' @endphp
+            <li class="flex items-center justify-center h-32 splide__slide bg-offWhite rounded-xl"><img src="{{ asset('media/brands/'. $img) }}" alt="{{$brand}}" class="h-full"></li>
+          @endforeach
         </ul>
       </div>
     </div>
   </div>
 </section>
 <section class="bg-offWhite">
-  <div class="container space mx-auto py-12">
-    PulsarNL
+  <div class="container py-12 mx-auto space">
+    <div class="flex flex-row justify-between">
+      <h3 class="text-xl font-bold text-mainPurple">PulsarNL</h3>
+      <a href="p-6" class="">View all ></a>
+    </div>
+    
   </div>
 </section>
 @endsection
